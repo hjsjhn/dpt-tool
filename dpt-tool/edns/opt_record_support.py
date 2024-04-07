a834506f20f5c3b04e005e63518712b1074197a5
hjsjhn <hjsjhn@outlook.com>
2024-04-07 16:28:48 +0800

    Feat: added partial as potential result for opt_record test

--- dpt-tool/edns/opt_record_support.py
+++ dpt-tool/edns/opt_record_support.py
@@ -1,6 +1,9 @@
+import time
 from utils import pydig, edns_opt_dict
 
-def check_dns_opt_record(server: str) -> bool:
+MAX_TRIES = 10
+
+def check_dns_opt_record_once(server: str, domain: str = "checkmydns.club") -> bool:
     """
     Make a DNS query to the specified DNS server and check if the response
     contains OPT records
@@ -14,9 +17,35 @@ def check_dns_opt_record(server: str) -> bool:
     Examples:
         `check_dns_opt_record('1.1.1.1', 'checkmydns.club')`
     """
-    response = pydig(["@" + server, "+edns"])
+    response = pydig(["@" + server, "+edns", ])
     try:
         ercode = response.section['ADDITIONAL'].optrr.ercode
         return ercode != 0
     except:
         return False
+    
+def check_dns_opt_record(server: str, domain: str = "checkmydns.club") -> bool:
+    """
+    Make DNS queries to the specified DNS server and check if the response
+    contains OPT records
+
+    Args:
+        `server`: the DNS server to query
+
+    Returns:
+        - `Yes` if the server supports OPT records always
+        - `No` if the server does not support OPT records always
+        - `Partial` if the server supports OPT records sometimes
+
+    Examples:
+        `check_dns_opt_record('1.1.1.1', 'checkmydns.club')`
+    """
+    las = None
+    ret = None
+    for _ in range(MAX_TRIES):
+        ret = "Yes" if check_dns_opt_record_once(server, domain) else "No"
+        if las is not None and las != ret:
+            return "Partial"
+        las = ret
+        time.sleep(1)
+    return ret
