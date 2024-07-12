245c2f8d0c144419e19fec7ebf03a71d706ac98a
hjsjhn <hjsjhn@outlook.com>
2024-07-12 17:19:29 +0800

    Feat: changed encoding20 check into a easier way

--- dpt-tool/measures_against_response_forgery/bit_0x20.py
+++ dpt-tool/measures_against_response_forgery/bit_0x20.py
@@ -6,38 +6,27 @@ from utils import pydig, edns_opt_dict
 from time import sleep
 import requests
 
-def check_0x20_encoding(server, sld='checkmydns.club', auth_server='108.61.171.88:88'):
+def check_0x20_encoding(server, domain='encoding20.checkmydns.club', specific_ip="127.0.0.1"):
     """
+    This function checks if the DNS server supports 0x20 encoding by querying the specific domain
+    Note that this function need support from the AuthDNS encoding20 plugin
+    Check if the specific IP is returned in the response to determine if the server supports 0x20 encoding
     Args:
         `server`: the DNS server to query
-        `sld`: the second level domain to check
-        `auth_server`: the authoritative server for the domain
+        `domain`: the specific domain for encoding20 plugin
+        `specific_ip`: the specific IP set in AuthDNS encoding20 plugin
 
     Returns:
         `True` if the server supports 0x20 encoding, `False` otherwise
 
     Examples:
-        `check_0x20_encoding("8.8.8.8", "checkmydns.club", "108.61.171.88")`
+        `check_0x20_encoding("8.8.8.8", "encoding20.checkmydns.club", "127.0.0.1")
     """
-    uuid_str = str(uuid.uuid4())
-    response = pydig(["@" + server, f"{uuid_str}.{sld}"])
-    sleep(1)
-    max_try = 3
-    while max_try > 0:
-        sleep(1)
-        try:
-            url = f"http://{auth_server}/data/{uuid_str}"
-            response = requests.get(url, timeout=(1,2))
-            if response.status_code == 200:
-                # get the json content from response
-                content = response.json()
-                if content["uuid"] == uuid_str:
-                    return True if content["0x20"] else False
-                break
-            else:
-                max_try -= 1
-        except:
-            max_try -= 1
+    response = pydig(["@" + server, domain, "A"])
+    if response.rcode == 0:
+        for answer in response.section['ANSWER'].record:
+            if answer.rrtype == "A" and answer.rdata == specific_ip:
+                return True
     return False
 
-# print(check_0x20_encoding("8.8.8.8", "checkmydns.club", "108.61.171.88:88"))
+# print(check_0x20_encoding("8.8.8.8", "encoding20.checkmydns.club", "127.0.0.1"))
