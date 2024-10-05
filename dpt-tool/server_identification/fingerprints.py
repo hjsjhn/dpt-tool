cff3e1ef98744e90bb95c194d82ed785766793a9
hjsjhn <hjsjhn@outlook.com>
2024-10-05 23:40:40 +0800

    Feat: added more scan tools in system fingerprint scanner module

--- dpt-tool/server_identification/fingerprints.py
+++ dpt-tool/server_identification/fingerprints.py
@@ -1,5 +1,6 @@
 import nmap
 import requests
+from utils import scan_ip_once
 
 def get_http_fingerprint(ip):
     url = f'http://{ip}'
@@ -13,24 +14,38 @@ def get_http_fingerprint(ip):
         }
         return fingerprint
     except requests.exceptions.Timeout:
-        return "Request timed out"
+        print("[ERR]Request timed out")
+        return False
     except requests.exceptions.RequestException as e:
-        return str(e)
+        print("[ERR]" + str(e))
+        return False
 
-def get_system_fingerprint(ip):
+def nmap_get_system_fingerprint(ip):
     nm = nmap.PortScanner()
     try:
         nm.scan(ip, arguments='-O')  # -O 参数用于操作系统检测
-        if 'osclass' in nm[ip]:
+        if 'osclass' in nm[ip] and len(nm[ip]['osclass']) > 0:
             os_fingerprint = nm[ip]['osclass']
-            return os_fingerprint
-        elif 'osmatch' in nm[ip]:
+            return (True, os_fingerprint)
+        elif 'osmatch' in nm[ip] and len(nm[ip]['osmatch']) > 0:
             os_fingerprint = nm[ip]['osmatch']
-            return os_fingerprint
+            print(os_fingerprint)
+            return (True, os_fingerprint)
         else:
-            return "No OS information found"
+            return (False, "No OS information found")
     except Exception as e:
-        return str(e)
+        return (False, str(e))
+
+def get_system_fingerprint(ip):
+    res_nmap = nmap_get_system_fingerprint(ip)
+    res_dnssoftver = scan_ip_once(ip, "vendor")
+    if res_nmap[0]:
+        return res_nmap[1]
+    elif len(res_dnssoftver) > 0:
+        return ','.join(res_dnssoftver)
+    else:
+        return "No OS information found"
+
 
 # ip_address = '208.67.222.222'
 # http_fingerprint = get_http_fingerprint(ip_address)
--- dpt-tool/utils/__init__.py
+++ dpt-tool/utils/__init__.py
@@ -1,2 +1,3 @@
 from .pydig4dpt import pydig
+from .dnssoftver4dpt import scan_ip_once
 from .constant import edns_opt_dict, dnskey_algo_dict
